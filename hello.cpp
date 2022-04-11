fc446f4edc61ad077099e7029cde11ee65c16573
G8-2110990159 <101543794+G8-2110990159@users.noreply.github.com>
2022-04-11 22:36:26 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,12 @@
-hi hello 
+<!DOCTYPE HTML>
+           <HEAD>
+		   <title> javascript test-1</title>
+           </HEAD>
+          <body>
+		  <script type="text/javascript">
+		  for(var i=0; i<=5; i++){
+		  alert(i);
+		  }
+		  </script>
+		  </body> 
+</ HTML >
